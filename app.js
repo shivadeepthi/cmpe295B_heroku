7bd52d5280809f1dfcf1890cbe4d01abaabbe872
shivadeepthi <shivadeepthitoopran@gmail.com>
2015-12-03 13:37:57 -0800

    herokucliuduh346t

--- app.js
+++ app.js
@@ -514,10 +514,10 @@ var io = require('socket.io').listen(server.listen(port,function(){
 	SensorTag.discover(onDiscover);
 })
 );
-//io.on('connection', function () {
-//	  io.set("transports", ["xhr-polling"]);
-//	  io.set("polling duration", 100);
-//	});
+io.on('connection', function () {
+	  io.set("transports", ["xhr-polling"]);
+	  io.set("polling duration", 100);
+	});
 
 
 
